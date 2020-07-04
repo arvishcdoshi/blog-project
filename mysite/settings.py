ff2af1d8f853706e3d053e4872136c021aae1156
arvishcdoshi <doshi.arvish16@gmail.com>
2020-07-04 16:25:41 +0530

    Added template

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$6z5=9%odigy-t0vq!c!phe6y=^&!!unlr!bf9wpxs5)0aoz3$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
